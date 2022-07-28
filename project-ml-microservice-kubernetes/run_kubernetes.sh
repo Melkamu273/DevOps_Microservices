03b17898bd25b006106894929aa9e8664f12dea7
Melkamu273 <melkamutessema372@gmail.com>
2022-07-28 22:58:54 +0300

    run docker file works well

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=melkamu372/housing-price-predictor
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictor-app --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/predictor-app 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=melkamu372/housing-price-predictor
 
 # Step 2:  
 # Authenticate & tag
+echo ****** | docker login --username melkamu372 --password-stdin
+docker image tag ml-predictor $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
